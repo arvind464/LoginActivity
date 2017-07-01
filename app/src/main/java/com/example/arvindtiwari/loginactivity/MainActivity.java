6c42795063f81eb2fa23057452af60b6d5084a24
Arvind Tiwari <tarvind464@gmail.com>
2017-07-01 12:02:20 +0530

    done login page

--- app/src/main/java/com/example/arvindtiwari/loginactivity/MainActivity.java
+++ app/src/main/java/com/example/arvindtiwari/loginactivity/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.arvindtiwari.loginactivity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set View to register.xml
         setContentView(R.layout.activity_main);
 
-        //dsfgjhoiuioytrxhf
+        TextView loginScreen = (TextView) findViewById(R.id.link_to_login);
+
+        // Listening to Login Screen link
+        loginScreen.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                // Closing registration screen
+                // Switching to Login Screen/closing register screen
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/arvindtiwari/loginactivity/RegisterActivity.java
+++
@@ -1,8 +0,0 @@
-package com.example.arvindtiwari.loginactivity;
-
-/**
- * Created by Arvind Tiwari on 6/27/2017.
- */
-
-public class RegisterActivity {
-}
